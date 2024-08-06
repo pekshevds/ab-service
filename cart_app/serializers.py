42f9f91a17423c53c51e9ac7e9a1933d8c1c0134
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-08-06 12:04:18 +0300

    refactoring

--- cart_app/serializers.py
+++ cart_app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from catalog_app.serializers import GoodSerializer
 
 
 class UserSerializer(serializers.Serializer):
@@ -8,5 +9,10 @@ class UserSerializer(serializers.Serializer):
 
 
 class CartSerializer(serializers.Serializer):
+    good = GoodSerializer()
+    qnt = serializers.DecimalField(max_digits=15, decimal_places=3)
+
+
+class SimpleCartSerializer(serializers.Serializer):
     good_id = serializers.UUIDField()
     qnt = serializers.DecimalField(max_digits=15, decimal_places=3)
